b6ee6e06e107f37a1159b12db2d4b97cecfe3b57
Juan Schallmoser <schallmoserjuan@gmail.com>
2025-11-04 20:27:38 -0300

    Add ScrollToTop component and integrate it into Layout for improved navigation

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
 import styles from "./Layout.module.css";
+import ScrollToTop from '../ScrollToTop/ScrollToTop';
 
 const Layout = ({ children }) => {
   return (
     <div className={styles.layout}>
       <Sidebar />
+      <ScrollToTop />
       <main className={styles.containerRight}>
         <div className={styles.themeToggleContainer} aria-hidden="false">
           <ThemeToggle />
